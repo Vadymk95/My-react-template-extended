4f91265b28b4979194c9b34a798024710bc499b1
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-11-01 10:27:15 +0200

    set up i18n, zustland, axios

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.tsx';
+import './i18n';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <StrictMode>
+    <StrictMode>
+        <BrowserRouter>
             <App />
-        </StrictMode>
-    </BrowserRouter>
+        </BrowserRouter>
+    </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import eslint from 'vite-plugin-eslint2';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+    server: {
+        port: 3000
+    },
     plugins: [react(), eslint(), svgr()],
     base: './',
     build: {
